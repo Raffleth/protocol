209876c06a49120c1a376b8d229d883834e1feec
ubinatus <51177379+ubinatus@users.noreply.github.com>
2024-05-09 16:29:19 -0500

    feat: add arbitrum chainlink config

--- script/ChainlinkConfig.sol
+++ script/ChainlinkConfig.sol
@@ -14,6 +14,7 @@ contract ChainlinkConfig {
 
     function getActiveNetworkChainlinkConfig() public returns (NetworkConfig memory activeNetworkConfig) {
         chainIdToNetworkConfig[11_155_111] = getSepoliaEthConfig();
+        chainIdToNetworkConfig[42_161] = getArbitrumConfig();
         chainIdToNetworkConfig[31_337] = getAnvilEthConfig();
 
         activeNetworkConfig = chainIdToNetworkConfig[block.chainid];
@@ -27,6 +28,16 @@ contract ChainlinkConfig {
             subscriptionId: 64_336_788_156_056_022_578_152_949_903_769_173_775_874_862_968_421_238_424_076_813_306_884_208_832_258,
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae
+        });
+    }
+
+    function getArbitrumConfig() internal pure returns (NetworkConfig memory sepoliaNetworkConfig) {
+        sepoliaNetworkConfig = NetworkConfig({
+            //  solhint-disable-next-line max-line-length
+            subscriptionId: 71_129_791_559_647_049_751_166_775_094_489_245_575_269_316_441_732_220_439_993_240_353_092_676_987_328,
+            vrfCoordinator: 0x3C0Ca683b403E37668AE3DC4FB62F4B29B6f7a3e,
+            // 150 gwei key hash
+            keyHash: 0xe9f223d7d83ec85c4f78042a4845af3a1c8df7757b4997b815ce4b8d07aca68c
         });
     }
 
